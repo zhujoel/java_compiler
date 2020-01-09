2a6d1a3e5986e8f78b88516914b25e359d83bd3c
Antoine FLICHY <antoine.flichy@grenoble-inp.org>
2020-01-09 22:22:17 +0100

    modif perso.sh

--- src/test/script/perso.sh
+++ src/test/script/perso.sh
@@ -1,4 +1,4 @@
 cd "$(dirname "$0")"/../../.. || exit 1
 
 PATH=./src/test/script/launchers:"$PATH"
-test_perso src/test/deca/syntax/invalid/provided/simple_lex.deca
+test_lex src/test/deca/syntax/invalid/provided/simple_lex.deca
