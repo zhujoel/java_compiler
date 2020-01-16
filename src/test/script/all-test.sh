f7ff80c473614a6a3bde9d5426fdd703497b18ad
Thomas Cassagnes <Thomas.Cassagnes@grenoble-inp.org>
2020-01-16 17:54:41 +0100

    test cobertura

--- src/test/script/all-test.sh
+++ src/test/script/all-test.sh
@@ -8,7 +8,7 @@ basic-decac.sh
 "$DECAC_HOME"/src/test/script/launchers/chain_test_context "$DECAC_HOME"/src/test/deca/context/{valid,invalid}/created/*
 "$DECAC_HOME"/src/test/script/launchers/validor_lex
 "$DECAC_HOME"/src/test/script/launchers/invalidor_lex
-#basic-gencode.sh
-#basic-synt
+basic-gencode.sh
+basic-synt
 #common-tests
-#perso.sh
+perso.sh
