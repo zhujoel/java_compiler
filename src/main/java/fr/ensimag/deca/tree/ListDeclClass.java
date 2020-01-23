e227000b45c6fe2ac40eefd6b9b266d1c16613d2
Joël ZHU <Joel.Zhu@ensimag.grenoble-inp.fr>
2020-01-23 10:28:03 +0100

    Etape C, génération code pour la déclaration des méthodes + ajout de tests

--- src/main/java/fr/ensimag/deca/tree/ListDeclClass.java
+++ src/main/java/fr/ensimag/deca/tree/ListDeclClass.java
@@ -3,12 +3,15 @@ package fr.ensimag.deca.tree;
 import org.apache.log4j.Logger;
 
 import fr.ensimag.deca.DecacCompiler;
-import fr.ensimag.deca.context.ClassType;
 import fr.ensimag.deca.context.ContextualError;
-import fr.ensimag.deca.context.Type;
 import fr.ensimag.deca.tools.IndentPrintStream;
+import fr.ensimag.ima.pseudocode.Label;
+import fr.ensimag.ima.pseudocode.LabelOperand;
+import fr.ensimag.ima.pseudocode.NullOperand;
 import fr.ensimag.ima.pseudocode.Register;
 import fr.ensimag.ima.pseudocode.RegisterOffset;
+import fr.ensimag.ima.pseudocode.instructions.LOAD;
+import fr.ensimag.ima.pseudocode.instructions.STORE;
 
 /**
  * Liste des classes d'un programme.
@@ -53,8 +56,21 @@ public class ListDeclClass extends TreeList<AbstractDeclClass> {
     }
     public void codeGenListClass(DecacCompiler compiler) {
         
-        compiler.getEnvironmentClass().put(compiler.getType("Object").getName(), new RegisterOffset(1, Register.GB));
+    	// on génère le code pour la classe Object à la main car celle-ci n'est pas déclaré
+        compiler.addComment("Code de la table des méthodes de Object");
+        compiler.addInstruction(new LOAD(new NullOperand(), Register.getR(0)));
+        compiler.addInstruction(new STORE(Register.getR(0), new RegisterOffset(compiler.getStackManager().getStackCpt(), Register.GB)));
+        compiler.getStackManager().addStackCpt();
         
+        Label objEq0 = new Label("code.Object.equals");
+        compiler.addInstruction(new LOAD(new LabelOperand(objEq0), Register.getR(0)));
+        compiler.addInstruction(new STORE(Register.getR(0), new RegisterOffset(compiler.getStackManager().getStackCpt(), Register.GB)));
+        compiler.getStackManager().addStackCpt();
+		
+        compiler.addLabel(objEq0);
+        
+        compiler.getEnvironmentClass().put(compiler.getType("Object").getName(), new RegisterOffset(1, Register.GB));
+
         for (AbstractDeclClass i : getList()) {
             i.codeGenDeclClass(compiler);
         }
