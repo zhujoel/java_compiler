bc79533207e7dbaafadcf4a3b07136f7d8177891
Antoine FLICHY <antoine.flichy@grenoble-inp.org>
2020-01-30 10:35:27 +0100

    [ANALYSE CONTEXTUELLE]
    - correction du bug de comparaison entre objets

--- src/main/java/fr/ensimag/deca/tree/AbstractOpExactCmp.java
+++ src/main/java/fr/ensimag/deca/tree/AbstractOpExactCmp.java
@@ -1,5 +1,10 @@
 package fr.ensimag.deca.tree;
 
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.context.ClassDefinition;
+import fr.ensimag.deca.context.ContextualError;
+import fr.ensimag.deca.context.EnvironmentExp;
+import fr.ensimag.deca.context.Type;
 
 /**
  *
@@ -10,6 +15,23 @@ public abstract class AbstractOpExactCmp extends AbstractOpCmp {
 
     public AbstractOpExactCmp(AbstractExpr leftOperand, AbstractExpr rightOperand) {
         super(leftOperand, rightOperand);
+    }
+    
+    @Override
+    public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
+            ClassDefinition currentClass) throws ContextualError {
+    	Type type1 = this.getLeftOperand().verifyExpr(compiler, localEnv, currentClass);
+        Type type2 = this.getRightOperand().verifyExpr(compiler, localEnv, currentClass);
+        
+        if (type1.sameType(type2)) {
+        	this.setType(compiler.getType("boolean"));
+        	return this.getType();
+        } else if(type1.isClass() && type2.isClass()) {
+        	this.setType(compiler.getType("boolean"));
+        	return this.getType();
+        }
+        throw new ContextualError("[SyntaxeContextuelle]Comparison between two different types (" 
+        + type1.toString() + " & " + type2.toString() + ")", getLocation());
     }
 
 
